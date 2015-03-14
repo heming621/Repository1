b150c9f3e57df3211cdbadea597d5ce1e8535c49
heming621 <heming585@gmail.com>
2015-03-14 21:30:22 +0800

    add avro_test

---
+++ avro_test/compile.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+javac -classpath /home/zhm/avro_test/lib/avro-1.7.7.jar:/home/zhm/avro_test/lib/avro-tools-1.7.7.jar:/home/zhm/avro_test/lib/jackson-core-asl-1.9.13.jar:/home/zhm/avro_test/lib/jackson-mapper-asl-1.9.13.jar example/avro/User.java Test.java
---
+++ avro_test/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java  -classpath /home/zhm/avro_test/lib/avro-1.7.7.jar:/home/zhm/avro_test/lib/avro-tools-1.7.7.jar:/home/zhm/avro_test/lib/jackson-core-asl-1.9.13.jar:/home/zhm/avro_test/lib/jackson-mapper-asl-1.9.13.jar:User.jar:. Test
